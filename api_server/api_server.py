3274bd7eaf37c84d9b8c0f89dc8e0e44c8dd0cc3
aazat3 <atavlykaev@list.ru>
2025-02-14 23:10:29 +0500

    test

--- api_server/api_server.py
+++ api_server/api_server.py
@@ -6,13 +6,15 @@ app = Flask(__name__)
 
 @app.route('/api/weights', methods=['GET'])
 def get_weights():
-    db_path = Path(__file__).parent.parent / 'scale_server'  / 'weights.db'
+    # db_path = Path(__file__).parent.parent / 'scale_server'  / 'weights.db'
     
-    conn = sqlite3.connect("db_path")
-    cursor = conn.cursor()
-    cursor.execute("SELECT device_id, weight, timestamp FROM weight_data ORDER BY timestamp DESC LIMIT 10")
-    data = cursor.fetchall()
-    conn.close()
+    # conn = sqlite3.connect("db_path")
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT device_id, weight, timestamp FROM weight_data ORDER BY timestamp DESC LIMIT 10")
+    # data = cursor.fetchall()
+    # conn.close()
+
+    data = [(1, 30, '2025-02-14T10:00:00'), (2, 45, '2025-02-14T11:00:00')]
 
     return jsonify([{"device_id": d[0], "weight": d[1], "timestamp": d[2]} for d in data])
 
