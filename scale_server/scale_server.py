1066652c021d915ecc083e2b2ad4c9ba304c1bc9
aazat3 <atavlykaev@list.ru>
2025-02-20 00:30:20 +0500

    test

--- scale_server/scale_server.py
+++ scale_server/scale_server.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 # Подключение к базе данных (или создание)
 db_path = Path(__file__).parent.parent / 'database'  / 'weights.db'
 logger.info(db_path)
-conn = sqlite3.connect(db_path)
+conn = sqlite3.connect("asdasdasdasdasdasdsadasdasd")
 cursor = conn.cursor()
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS weight_data (
