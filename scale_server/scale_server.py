20655e9c68cb84663d194d6608015eed1f57f5cf
aazat3 <atavlykaev@list.ru>
2025-03-15 18:30:18 +0500

    change to aiomqtt

--- scale_server/scale_server.py
+++ scale_server/scale_server.py
@@ -1,14 +1,15 @@
 import sqlite3
-import paho.mqtt.client as mqtt
+import asyncio
+import aiomqtt
 # from pathlib import Path
 import logging
 from sqlalchemy.orm import Session
 import json
+from SQL import database, models, schemas, crud
 
 # .\mosquitto_pub -h aazatserver.ru -t "iot/device1/weight" -m '{"name": "orange", "calories": 56}' -u "admin" -P "admin"
 # .\mosquitto_pub -h aazatserver.ru -t "iot/device1/weight" -m '{\"name\": \"orange\", \"calories\": 56}' -u "admin" -P "admin"
 
-from SQL import database, models, schemas, crud
 
 def save_to_db(payload):
     with database.SessionLocal() as db:
@@ -27,30 +28,41 @@ def save_to_db(payload):
             db.close()
         # crud.create_product(db, product)
 
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    async with aiomqtt.Client("aazatserver.ru",1883, "admin", "admin") as client:
+        await client.subscribe("iot/+/weight")
+        async for message in client.messages:
+            payload = json.loads(message.payload.decode())
+            save_to_db(payload)
+
+
+asyncio.run(main())
+
 # Callback при подключении к брокеру
-def on_connect(client, userdata, flags, reason_code, properties):
-    logger.info("Connected with result code " + str(reason_code))
-    client.subscribe("iot/+/weight")  # Подписка на все устройства
+# def on_connect(client, userdata, flags, reason_code, properties):
+#     logger.info("Connected with result code " + str(reason_code))
+#     client.subscribe("iot/+/weight")  # Подписка на все устройства
 
 
-# Функция обработки сообщений
-def on_message(client, userdata, msg):
-    logger.info("get message")
-    logger.info(msg.payload.decode())
+# # Функция обработки сообщений
+# def on_message(client, userdata, msg):
+#     logger.info("get message")
+#     logger.info(msg.payload.decode())
 
-    # device_id = msg.topic.split("/")[1]
-    # weight = float(msg.payload.decode())
-    payload = json.loads(msg.payload.decode())
-    save_to_db(payload)
+#     # device_id = msg.topic.split("/")[1]
+#     # weight = float(msg.payload.decode())
+#     payload = json.loads(msg.payload.decode())
+#     save_to_db(payload)
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
-# Создание MQTT-клиента
-client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
-client.on_connect = on_connect
-# client.on_connect = lambda c, u, f, rc: c.subscribe("iot/+/weight")
-client.on_message = on_message
-client.username_pw_set("admin", "admin")
-client.connect("aazatserver.ru", 1883, 60)  # Подключение к брокеру Mosquitto
-client.loop_forever()  # Запуск бесконечного цикла
+# # Создание MQTT-клиента
+# client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+# client.on_connect = on_connect
+# # client.on_connect = lambda c, u, f, rc: c.subscribe("iot/+/weight")
+# client.on_message = on_message
+# client.username_pw_set("admin", "admin")
+# client.connect("aazatserver.ru", 1883, 60)  # Подключение к брокеру Mosquitto
+# client.loop_forever()  # Запуск бесконечного цикла
