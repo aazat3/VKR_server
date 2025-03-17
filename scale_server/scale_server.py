a29a0a76fb402b70781453115bbd3c164aff078d
aazat3 <atavlykaev@list.ru>
2025-03-17 16:54:58 +0500

    fix sample rate

--- scale_server/scale_server.py
+++ scale_server/scale_server.py
@@ -153,7 +153,7 @@ async def main():
 
     args.model_path = os.environ.get('VOSK_MODEL_PATH', 'model')
     args.spk_model_path = os.environ.get('VOSK_SPK_MODEL_PATH')
-    args.sample_rate = float(os.environ.get('VOSK_SAMPLE_RATE', 8000))
+    args.sample_rate = float(os.environ.get('VOSK_SAMPLE_RATE', 16000))
     args.max_alternatives = int(os.environ.get('VOSK_ALTERNATIVES', 0))
     args.show_words = bool(os.environ.get('VOSK_SHOW_WORDS', True))
 
