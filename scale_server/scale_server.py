3741c1f3e437bb00e56958ebe6476e1868f42da1
aazat3 <atavlykaev@list.ru>
2025-04-03 21:00:53 +0500

    first test websocket

--- scale_server/scale_server.py
+++ scale_server/scale_server.py
@@ -14,13 +14,12 @@ from datetime import datetime, timezone
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-device_tasks = {}  # Словарь для хранения задач устройств
-INACTIVITY_TIMEOUT = 7
-
+# Таймауты и другие параметры
+CONNECTION_TIMEOUT = 10  # Время ожидания активности от клиента в секундах
+INACTIVITY_TIMEOUT = 10  # Время неактивности соединения до его закрытия
 
 def process_chunk(rec, payload):    
     try:
-        # logging.info(payload)
         if payload == '{"eof" : 1}':
             return rec.FinalResult(), True
         if payload == '{"reset" : 1}':
@@ -35,9 +34,9 @@ def process_chunk(rec, payload):
         return '{"error": "processing error"}', False
     
 
-async def recognize(client_id, message_queue):
+async def recognize(websocket, path):
     """Обрабатывает сообщения от конкретного IoT-устройства"""
-    logging.info(f"✅ Начало обработки устройства {client_id}")
+    logging.info(f"✅ Начало обработки устройства {websocket.remote_address}")
     
     global model
     global spk_model
@@ -51,17 +50,12 @@ async def recognize(client_id, message_queue):
     show_words = args.show_words
     max_alternatives = args.max_alternatives
     audio_data = bytearray()
-
-    while True:
+    
+    async for message in websocket:
         try:
-             # Ждём новое сообщение с тайм-аутом
-            message = await asyncio.wait_for(message_queue.get(), timeout=INACTIVITY_TIMEOUT)
-            payload = message.payload
-            last_message_time = datetime.now(timezone.utc)  # Текущее время в UTC
-            
             # Load configuration if provided
-            if isinstance(payload, str) and 'config' in payload:
-                jobj = json.loads(payload)['config']
+            if isinstance(message, str) and 'config' in message:
+                jobj = json.loads(message)['config']
                 logging.info("Config %s", jobj)
                 if 'phrase_list' in jobj:
                     phrase_list = jobj['phrase_list']
@@ -88,23 +82,33 @@ async def recognize(client_id, message_queue):
                 if spk_model:
                     rec.SetSpkModel(spk_model)
 
-            response = await loop.run_in_executor(pool, process_chunk, rec, payload)
-            audio_data.extend(payload)
+            response = await loop.run_in_executor(pool, process_chunk, rec, message)
+            audio_data.extend(message)
 
             # logging.info(response[0])
             if response[1]: break
-
-        except asyncio.TimeoutError:
-            # Если прошло слишком много времени без сообщений — завершаем задачу
-            logging.info(f"⚠ Завершаем {client_id} (неактивен {INACTIVITY_TIMEOUT} сек.)")
+        except websockets.exceptions.ConnectionClosedError:
+            logging.info(f"Соединение закрыто: {websocket.remote_address}")
+        except Exception as e:
+            logging.error(f"Ошибка: {e}")
+        finally:
             save_wav(audio_data)
-            audio_data.clear()  # Очищаем массив данных после записи в WAV
-            break
+            del audio_data  # Явное освобождение памяти
+            logging.info(f"⚠ Завершаем {websocket.remote_address}")
 
-    # Очистка после завершения
-    del device_tasks[client_id]
-    del message_queue  # Явно удаляем очередь (необязательно, но можно)
-    logging.info(f"🛑 Задача {client_id} завершена")
+        # except asyncio.TimeoutError:
+        #     # Если прошло слишком много времени без сообщений — завершаем задачу
+        #     logging.info(f"⚠ Завершаем {client_id} (неактивен {INACTIVITY_TIMEOUT} сек.)")
+        #     save_wav(audio_data)
+        #     audio_data.clear()  # Очищаем массив данных после записи в WAV
+        #     break
+        # logging.info(f"⚠ Завершаем {websocket.remote_address}")
+        # save_wav(audio_data)
+        # audio_data.clear()  # Очищаем массив данных после записи в WAV
+    # # Очистка после завершения
+    # del device_tasks[client_id]
+    # del message_queue  # Явно удаляем очередь (необязательно, но можно)
+    # logging.info(f"🛑 Задача {client_id} завершена")
 
 
 # Функция для сохранения аудиоданных в WAV
@@ -152,6 +156,8 @@ async def main():
     
     args = type('', (), {})()
 
+    args.interface = os.environ.get('VOSK_SERVER_INTERFACE', '0.0.0.0')
+    args.port = int(os.environ.get('VOSK_SERVER_PORT', 2700))
     args.model_path = os.environ.get('VOSK_MODEL_PATH', 'model')
     args.spk_model_path = os.environ.get('VOSK_SPK_MODEL_PATH')
     args.sample_rate = float(os.environ.get('VOSK_SAMPLE_RATE', 16000))
@@ -170,41 +176,13 @@ async def main():
     spk_model = SpkModel(args.spk_model_path) if args.spk_model_path else None
     pool = concurrent.futures.ThreadPoolExecutor((os.cpu_count() or 1))
 
-    async with websockets.serve(recognize, args.interface, args.port):
-        await asyncio.Future()
+    try:
+        async with websockets.serve(recognize, args.interface, args.port):
+            await asyncio.Future()
+    finally:
+        pool.shutdown(wait=True)  # Корректное завершение ThreadPool
+        if 'model' in globals():
+            del model  # Освобождение памяти модели
 
-    # while True:
-    #     try:
-    #         async with aiomqtt.Client(
-    #             hostname="aazatserver.ru",
-    #             port=1883,
-    #             username="admin",
-    #             password="admin"
-    #         ) as client:
-                
-    #             await client.subscribe("iot/+/audio")
-    #             await client.subscribe("+/stream/voice")
-    #             recognizer = KaldiRecognizer(model, args.sample_rate)
-    #             async for message in client.messages:
-    #                 topic = message.topic
-    #                 payload = message.payload
-    #                 client_id = str(message.topic).split("/")[-2]
-    #                 if str(topic).endswith('/voice'):
-    #                     if recognizer.AcceptWaveform(payload):
-    #                         transcribe = recognizer.Result()
-    #                         data = json.loads(transcribe)
-    #                         logging.info(data)
-
-    #                 if str(topic).endswith('/audio'):
-    #                     if client_id not in device_tasks:
-    #                         logging.info("New device" + client_id)
-    #                         message_queue = asyncio.Queue()
-    #                         device_tasks[client_id] = asyncio.create_task(handle_device(client_id, message_queue))
-    #                     await message_queue.put(message)
-
-    #     except Exception as e:
-    #         logging.exception(f"Ошибка MQTT-соединения: {e}")
-    #         await asyncio.sleep(5)  # Ждём перед повторным подключением
-
-
+   
 asyncio.run(main())
