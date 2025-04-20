736cc1e5a7d7c109dccc95cd63d3192b7df5e83c
aazat3 <atavlykaev@list.ru>
2025-04-21 00:45:38 +0500

    fix

--- api_server/auth.py
+++ api_server/auth.py
@@ -2,7 +2,7 @@ from passlib.context import CryptContext
 
 from jose import jwt
 from datetime import datetime, timedelta, timezone
-from config import get_auth_data
+from config import *
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -19,6 +19,6 @@ def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(days=30)
     to_encode.update({"exp": expire})
-    auth_data = get_auth_data()
+    auth_data = Settings.get_auth_data()
     encode_jwt = jwt.encode(to_encode, auth_data['secret_key'], algorithm=auth_data['algorithm'])
     return encode_jwt
