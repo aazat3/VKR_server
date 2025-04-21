0ef22706f9cd4ed7cc6597346ca1c0232e642e14
aazat3 <atavlykaev@list.ru>
2025-04-21 18:42:45 +0500

    test

--- api_server/api_server.py
+++ api_server/api_server.py
@@ -65,8 +65,9 @@ def get_token(request: Request):
 def get_token_from_cookie_or_header(
     request: Request,
     token_from_header: str = Depends(oauth2_scheme),
-    users_access_token: str = Cookie(default=None, alias="users_access_token")
+    # users_access_token: str = Cookie(default=None, alias="users_access_token")
 ):
+    users_access_token = request.cookies.get('users_access_token')
     # Приоритет: Cookie -> Header
     token = users_access_token or token_from_header
     if not token:
