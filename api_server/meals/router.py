b6c3e4b54fe92f55bfb50d3b7f315db3888370d2
aazat3 <atavlykaev@list.ru>
2025-04-22 18:51:00 +0500

    test

--- api_server/meals/router.py
+++ api_server/meals/router.py
@@ -4,7 +4,7 @@ from fastapi.security import OAuth2PasswordBearer
 # from pathlib import Path
 import logging
 
-from api_server.user_auth.auth import get_current_user
+from user_auth.auth import get_current_user
 
 
 from SQL.models import *
