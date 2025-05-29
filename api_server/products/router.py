a630de7b374ecdd1fe76eb29f30e7f5c2209aa61
aazat3 <atavlykaev@list.ru>
2025-05-29 23:13:07 +0500

    fix

--- api_server/products/router.py
+++ api_server/products/router.py
@@ -33,7 +33,11 @@ async def add_products(
         protein_percent=product.protein_percent,
         fat_percent=product.fat_percent,
         carbohydrates_percent=product.carbohydrates_percent,
-        **product.model_dump()
+        **product.model_dump(exclude={
+            "categoryID", "name", "source_type_id", 
+            "energy_kcal", "protein_percent", 
+            "fat_percent", "carbohydrates_percent"
+        })
     )
     result = await ProductsDAO.add_product(new_product)
     return result
